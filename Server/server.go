e5c5cb09e4a6189f5efdb1088927117e19faa8d3
JiangFeng <you@example.com>
2015-08-19 17:17:48 +0800

    功能成功

--- Server/server.go
+++ Server/server.go
@@ -6,9 +6,16 @@ import (
 	"log"
 	"net"
 	"os"
+	"runtime"
+	//	"strconv"
+	//	"time"
 )
 
 func main() {
+	//number of core
+	var MULTICORE int = runtime.NumCPU() //number of core
+	log.Println("CORE_NUM:", MULTICORE)
+	runtime.GOMAXPROCS(MULTICORE)
 	//Listen the port of Server
 	netListener, err := net.Listen("tcp", ":22333")
 	CheckError(err)
@@ -20,18 +27,21 @@ func main() {
 		if err != nil {
 			continue
 		}
-		log.Println(conn.RemoteAddr().String(), "connect to client success by tcp ")
-		go handleConnection(conn)
+		log.Println("ClientAddr:", conn.RemoteAddr().String())
+		log.Println("Msg:connect to client success by tcp ")
+		go handleConnection(conn) //此处使用go关键字新建线程处理连接，实现并发
 		//from server to clients
-		//		ServerInfoPush(conn.RemoteAddr().String())//
-
+		//		ServerInfoPush(conn.RemoteAddr().String()) //
+		//		TcpSender(conn)
 	}
+	//	TcpSender(conn)
 
 	fmt.Println("Hello World!")
 }
 func CheckError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		//		log.Println(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 	}
 }
@@ -47,6 +57,7 @@ func handleConnection(conn net.Conn) {
 		log.Println(conn.RemoteAddr().String(), "receive data length:", l)
 		log.Println(conn.RemoteAddr().String(), "receive data: ", buffer[:l])
 		log.Println(conn.RemoteAddr().String(), "receive data string: ", string(buffer[:l]))
+		go loopingCall(conn)
 	}
 
 }
